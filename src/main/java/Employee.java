ac0e407284d54b8d407d8cf9cf33cbd90c984a50
marbin701 <marvin07012002@gmail.com>
2026-04-21 10:36:53 -0400

    employee is set up with getters and setters

---
+++ src/main/java/Employee.java
@@ -0,0 +1,41 @@
+public class Employee {
+
+    private int id;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    public
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String line = "30|Brittany Thibbs|40|16.50";
+        String[] parts= line.split("\\|");
+
+        int id =
+
+
+    }
+}
